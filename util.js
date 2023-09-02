51db87dd7e0e6b7146a81032f06013696c171c2b
rajnandini190 <142484988+rajnandini190@users.noreply.github.com>
2023-09-02 15:21:37 +0530

    flux.js

--- util.js
+++ util.js
@@ -1,14 +1,149 @@
-// pure function
+
+const Dispatcher = {
+    callbacks: [], 
+
+    register(callback){
+        this.callbacks.push(callback);
+    },
+
+    dispatch(action){
+        this.callbacks.forEach(callback => callback(action))
+    }
+};
+
+
+const File_Folder_Store ={
+    state:[
+        {
+            type:"folder",
+            name:"Root",
+            id: unique_id(),
+            level:0,
+            children:[
+                
+            ]
+        }
+     ]
+    ,
+
+    getState(){
+        return this.state;
+    }
+    ,
+    handleAction(Action){
+        if(Action.type === "AddFolder"){
+            
+            Iterate_Children_And_Push(Action.parent_id , this.state , Action.folderobj)
+            console.log("after element adding "  , this.state)
+        }
+        else if(Action.type === "AddFile"){
+            Iterate_Children_And_Push(Action.parent_id , this.state , Action.fileobj) 
+         
+        }
+        else if(Action.type === "Delete"){
+            
+            deleteNode(Action.parentid , File_Folder_Store.state , Action.delete_child_id )
+            console.log("updated state after delete " , this.state)
+        }
+    },
+
+    emitChange(){
+        console.log("store changes " , this.state)
+    }
+}
+
+
+const ExplorerView = {
+    init(){
+        console.log("init function call");
+        counter=0;
+        this.rootElement = document.getElementById("main");
+        console.log( "root element  " , this.rootElement)
+        this.render();
+    },
+
+    render(){
+        console.log("render function call");
+        const dataStore = File_Folder_Store.getState();
+        this.rootElement.innerHTML = "";
+        counter = 0 ;
+        Iterate_Children_for_print(this.rootElement , dataStore);
+    }
+}
+
+// Action Creators
+function Add_Folder_Action(folderobj , parent_id){
+    return {
+        type : "AddFolder",
+        folderobj,
+        parent_id
+    };
+}
+function Add_File_Action(fileobj , parent_id){
+    return {
+        type : "AddFile",
+        fileobj,
+        parent_id
+    }
+}
+function Delete_Action(parentid , delete_child_id){
+    return {
+        type : "Delete",
+        parentid,
+        delete_child_id
+    }
+}
+
+
+Dispatcher.register(File_Folder_Store.handleAction.bind(File_Folder_Store));
+
+
+ExplorerView.init();
+
+
+
+
+const onClickFolder = (event)=>{
+    
+    let inputValue = document.getElementById("InputValueId").value; 
+   
+    let parentObj = Iterate_Data_For_Getting_ParentObj(event.parentNode.id , File_Folder_Store.state);
+    let new_obj = createFolderNode(inputValue , "folder" , parentObj );
+    console.log("new object in onclick folder function " , new_obj);
+
+   
+    Dispatcher.dispatch(Add_Folder_Action(new_obj , parentObj.id));
+    ExplorerView.render()
+}  
+
+const onClickFile = (event)=>{
+      
+    let inputValue = document.getElementById("InputValueId").value;
+
+    let parentObj = Iterate_Data_For_Getting_ParentObj(event.parentNode.id , File_Folder_Store.state);
+    let new_obj = createFileNode(parentObj, inputValue , "file" );
+    console.log("new object in onclick file function " , new_obj);
+  
+   
+    Dispatcher.dispatch(Add_File_Action(new_obj , parentObj.id));
+    ExplorerView.render()
+     
+}
+
+const onClickDelete = (event) =>{ 
+    let parentId = event.parentNode.parentNode.id;
+    console.log(parentId)
+    Dispatcher.dispatch(Delete_Action(parentId , event.parentNode.id));
+    ExplorerView.render()
+}
 let parentid = 0;
 
-// function for creating unique id ====================================================
 
 function unique_id(){
     parentid++;
     return parentid;
 }
 
-// functions for creating button and icons  =============================================
 
 
 function creating_button(){
@@ -37,7 +172,6 @@ function creating_arrow(){
     return  arrow_icon;
 }
 
-// functions for performing operation on list ========================================
 
 function createFileNode(parentObj, fileName , fileType ){
     let newObj ={
@@ -82,7 +216,6 @@ const deleteNode = (id, childrenArray , deleteId) => {
 }
 
 
-// functions for performing operation on DOM ===============================================
 
 function createFile(name , obj){
 
@@ -113,12 +246,12 @@ function createFolder(name , obj ){
     main_div.appendChild(file_button);
     main_div.appendChild(delete_button);
     
-    // for appending children
+    
     let children_div = document.createElement("div");
     children_div.id = unique_id();
     
     main_div.appendChild(children_div)
-    // console.log(main_div);
+    
     return main_div;
 }
 
