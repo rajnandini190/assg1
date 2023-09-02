be5cbb4d053c24c18bcc832da117679c75ab478c
rajnandini190 <142484988+rajnandini190@users.noreply.github.com>
2023-09-02 15:49:45 +0530

    index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// common bond and entry point to the javascript
-
-
-function myFunction(){
-    const middle_section = document.getElementById("main_section");
-    let main_div = document.getElementById("main")
-    let level = 0;
-  
-}
 
 
 
